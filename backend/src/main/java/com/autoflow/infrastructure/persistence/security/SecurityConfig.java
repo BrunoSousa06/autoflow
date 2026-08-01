355fc393727682c01254899f17d74971a077f976
Bruno Sousa <brunorsousa06@gmail.com>
2026-08-01 04:19:48 -0300

    feat:ajuste no health check

--- backend/src/main/java/com/autoflow/infrastructure/persistence/security/SecurityConfig.java
+++ backend/src/main/java/com/autoflow/infrastructure/persistence/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/public/**").permitAll()
                         .requestMatchers(
-                                "/actuator/health",
+                                "/actuator/health/*",
                                 "/v3/api-docs",
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html",
