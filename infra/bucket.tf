3db5f0afbeb3f6d0bf367bf578cf7ba7872ecad1
Bruno Sousa <brunorsousa06@gmail.com>
2026-08-22 19:37:39 -0300

    feat: ajuste no bucket

--- infra/bucket.tf
+++ infra/bucket.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "autoflow_bucket" {
-  bucket = "state-${var.project_name}-terraform"
-
-}
+#resource "aws_s3_bucket" "autoflow_bucket" {
+#  bucket = "state-${var.project_name}-terraform"
+#
+#}
